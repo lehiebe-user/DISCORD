870f4f730726fbda0b125531b89054d3cdc0ce6b
GeeZMoKeZ <47711378+GeeZMoKeZ@users.noreply.github.com>
2020-05-23 18:40:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,33 @@ bot.on("message", message => {
 	}
 
     }
+						     if(message.content.startsWith("!stats")){
+        let onlines = message.guild.members.cache.filter(({ presence }) => presence.status !== 'offline').size;
+        let totalmembers = message.guild.members.cache.size;
+        let totalservers = bot.guilds.cache.size;
+        let totalbots =  message.guild.members.cache.filter(member => member.user.bot).size;
+        let totalrole = message.guild.roles.cache.get('701156465515167755').members.map(member => member.user.tag).length;
+
+        const monembed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Statistiques')
+            .setURL('https://discord.js.org/')
+            .setAuthor('Mon Bot discord', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+            .setDescription('Voici les statistiques du serveur')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addFields(
+                { name: 'Nombre de membrs total', value: totalmembers, inline: true },
+                { name: 'Membres connéctés : ', value: onlines, inline: true },
+                { name: 'Nombre de serveurs auquel le bot appartient : ', value: totalservers, inline: true },
+                { name: 'Nombres de bots sur le serveur : ', value: totalbots, inline: true },
+                { name: 'Nombre d\'arrivants : ', value: totalrole, inline: true },
+            )
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        message.channel.send(monembed);
+    }
     
 })
 
